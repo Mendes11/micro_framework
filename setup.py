55f618d22ffcf403994715d86ce94d3f527bbd6a
Rafael Mendes <rafael.mendes@birmind.com.br>
2020-05-25 18:50:24 -0300

    Final adjustments due to bug with setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ import setuptools
 
 setuptools.setup(
     packages=setuptools.find_packages(),
-    exclude_package_data={"": ["README.md"]},
     name='micro-framework',
-    version='1.0.1',
+    url='https://github.com/Mendes11/micro_framework',
+    version='1.0.3',
     description='Framework to create CPU or IO bound microservices.',
-    long_description= 'file: README.rst',
+    long_description= 'file: README.md',
     author = 'Rafael Mendes Pacini Bachiega',
     author_email = 'rafaelmpb11@hotmail.com',
     classifiers =
-    ['Environment :: Web Environment',
-    'Intended Audience :: Developers',
+    ['Intended Audience :: Developers',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3 :: Only',
     'Programming Language :: Python :: 3.6',
