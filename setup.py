5d8cfe1b116c26d056e001d019093b5fa8dea836
Rafael Mendes <rafaelmpb11@hotmail.com>
2020-09-02 11:44:23 -0300

    Version 1.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     name='micro-framework',
     url='https://github.com/Mendes11/micro_framework',
-    version='1.2.0',
+    version='1.2.1',
     description='Framework to create CPU or IO bound microservices.',
     long_description= 'file: README.md',
     author = 'Rafael Mendes Pacini Bachiega',
