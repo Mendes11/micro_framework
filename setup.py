af7674450014bf142821d2a2ef74da7d8a07a86d
Rafael Mendes <rafaelmpb11@hotmail.com>
2021-02-01 23:48:24 -0300

    python 3.6 to classifiers in setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     ['Intended Audience :: Developers',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3 :: Only',
+    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9'],
