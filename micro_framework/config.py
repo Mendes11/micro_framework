f085e769e1f60b35dd6dae9ae85e9ff425aba482
Rafael Mendes <rafaelmpb11@hotmail.com>
2021-02-01 22:29:30 -0300

    Config is now merged with DefaultConfig on init

--- micro_framework/config.py
+++ micro_framework/config.py
@@ -1,15 +1,17 @@
 from collections import UserDict
 
+
 DEFAULT_CONFIG = dict(
     MAX_WORKERS=3,
-    WORKER_MODE='process',
-    MAX_TASKS_PER_CHILD=None,
+    WORKER_MODE='thread',
     AMPQ_URI='amqp://guest:guest@localhost:5672',
-    ENABLE_METRICS=False, # This is Preferable to avoid any port collision
-    METRICS = {
+    ENABLE_METRICS=False,  # This is Preferable to avoid any port collision
+    METRICS={
         'HOST': '0.0.0.0',
         'PORT': 8000,
     },
+    PROCESS_WORKER_TIMEOUT=10,
+    # After 10 seconds the worker will be turned off.
     WEBSOCKET_IP='0.0.0.0',
     WEBSOCKET_PORT=8765,
 )
@@ -21,18 +23,18 @@ class FrameworkConfig(UserDict):
     configurations when a config is set by the user
     """
 
-    def __init__(self, *args, default_config=None, **kwargs):
+    def _deep_update(self, config, default):
+        merged_dict = default.copy()
+        for name, val in config.items():
+            if name in default and isinstance(val, dict):
+                merged_val = self._deep_update(val, default[name])
+                merged_dict[name] = merged_val
+            else:
+                merged_dict[name] = val
+        return merged_dict
+
+    def __init__(self, config, default_config=None):
         if default_config is None:
             default_config = DEFAULT_CONFIG
-        self.default_config = default_config
-        super(FrameworkConfig, self).__init__(*args, **kwargs)
-
-    def __getitem__(self, item):
-        if item not in self.default_config:
-            return super(FrameworkConfig, self).__getitem__(item)
-
-        default_value = self.default_config.get(item)
-        value = self.data.get(item, default_value)
-        if isinstance(default_value, dict):
-            return FrameworkConfig(value, default_config=default_value)
-        return value
+        final_config = self._deep_update(config, default_config)
+        super(FrameworkConfig, self).__init__(final_config)
