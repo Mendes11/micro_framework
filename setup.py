3525aa5d6a4d3ec7aa535c6c828dd44c698d213a
Rafael Mendes <rafaelmpb11@hotmail.com>
2021-02-01 22:37:44 -0300

    Setup V2.0.1

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
 setuptools.setup(
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
     name='micro-framework',
     url='https://github.com/Mendes11/micro_framework',
-    version='2.0.0',
+    version='2.0.1',
     description='Framework to create CPU or IO bound microservices.',
     long_description= 'file: README.md',
     author = 'Rafael Mendes Pacini Bachiega',
@@ -13,11 +13,11 @@ setuptools.setup(
     ['Intended Audience :: Developers',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3 :: Only',
-    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8'],
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'],
     install_requires=[
         "kombu>=4.6.8", "websockets==8.1", "websocket-client==0.57.0",
-        "prometheus-client==0.8.0"
+        "prometheus-client==0.8.0", "loky==2.9.0"
     ],
 )
