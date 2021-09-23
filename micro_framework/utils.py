8f58c61d2c262e88d3ea664d3aa3f38a89fd371b
Rafael Mendes <rafaelmpb11@hotmail.com>
2021-09-23 20:22:23 -0300

    implemented get_dependency_standalone and get_dependency_standalone_sync to allow using the Framework dependencies outside it

--- micro_framework/utils.py
+++ micro_framework/utils.py
@@ -1,3 +1,6 @@
+import asyncio
+
+from micro_framework.config import FrameworkConfig
 from micro_framework.dependencies import RunnerDependency, Dependency
 
 
@@ -6,3 +9,53 @@ def is_runner_dependency(obj):
 
 def is_dependency(obj):
     return isinstance(obj, Dependency)
+
+
+async def get_dependency_standalone(provider: Dependency, worker_cls=None,
+                                    **configurations):
+    """
+    Returns an injected framework Dependency outside the scope of the
+    Framework.
+
+    It creates a FrameworkConfig from the given configurations (and default
+    ones defined by the framework itself). This configuration is used to
+    instantiate a worker, whose class is either given by the caller or
+    MockWorker bellow is used (which only stores config attribute).
+
+    :param Dependency provider: The Dependency Provider
+    :param worker_cls: Worker Class to instantiate with configurations
+    :param configurations: Additional Configurations to use in FrameworkConfig
+    :return: Injected Callable
+    """
+    config = FrameworkConfig(configurations)
+    provider.config = config
+
+    class MockWorker:
+        def __init__(self, config):
+            self.config = config
+
+    worker_cls = worker_cls or MockWorker
+    worker = worker_cls(config)
+    await provider.setup()
+    await provider.setup_dependency(worker)
+    return await provider.get_dependency(worker)
+
+
+def get_dependency_standalone_sync(provider: Dependency, worker_cls=None,
+                                   **configurations):
+    """
+    Do the same as :method:get_dependency_standalone but by creating a
+    temporary event loop prior to calling it.
+
+
+    :param Dependency provider: The Dependency Provider
+    :param worker_cls: Worker Class to instantiate with configurations
+    :param configurations: Additional Configurations to use in FrameworkConfig
+    :return: Injected Callable
+    """
+    loop = asyncio.new_event_loop()
+    return loop.run_until_complete(
+        get_dependency_standalone(
+            provider, worker_cls=worker_cls, **configurations
+        )
+    )
