b35e77afafcb1df01868bd1011f2c96d6fedb730
Rafael Mendes <rafaelmpb11@hotmail.com>
2021-10-25 22:35:08 -0300

    kombu requirement set to <6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9'],
     install_requires=[
-        "kombu>=4.6.8,<=5",
+        "kombu>=4.6.8,<6",
         "websockets>=8.1,<9",
         "websocket-client>=0.57.0,<1",
         "prometheus-client<0.9",
